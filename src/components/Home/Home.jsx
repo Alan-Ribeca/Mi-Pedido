502f6e6134c575e38238ceab5d46997f9afe8a01
Alan Ribeca <alanribeca2003@gmail.com>
2024-07-01 10:53:30 -0300

    empezando con un poco de logica

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,65 @@
+import { useState } from "react";
 import { Carrusel } from "./Carrusel";
 import "./home.scss";
 
 export const Home = () => {
+  const arrayProduc = [
+    {
+      id: 1,
+      img: "./img/public4Perf.jpeg",
+      alt: "img del producto",
+      nameProduc: "Nombre del producto",
+      stock: 500,
+      precio: 600,
+    },
+    {
+      id: 2,
+      img: "./img/i1.jpg",
+      alt: "img del producto",
+      nameProduc: "Nombre del producto",
+      stock: 30,
+      precio: 1600,
+    },
+    {
+      id: 3,
+      img: "./img/i2.jpg",
+      alt: "img del producto",
+      nameProduc: "Nombre del producto",
+      stock: 80,
+      precio: 80,
+    },
+    {
+      id: 4,
+      img: "./img/i3.jpg",
+      alt: "img del producto",
+      nameProduc: "Nombre del producto",
+      stock: 10,
+      precio: 350,
+    },
+    {
+      id: 5,
+      img: "./img/public4Perf.jpeg",
+      alt: "img del producto",
+      nameProduc: "Nombre del producto",
+      stock: 511,
+      precio: 556,
+    },
+    {
+      id: 6,
+      img: "./img/i1.jpg",
+      alt: "img del producto",
+      nameProduc: "Nombre del producto",
+      stock: 77,
+      precio: 330,
+    },
+  ];
+
+  const [abrirFiltrar, SetAbrirFiltrar] = useState();
+
+  function handleFiltrar() {
+    SetAbrirFiltrar(!abrirFiltrar);
+  }
+
   return (
     <>
       <section className="homeContainer">
@@ -12,7 +70,7 @@ export const Home = () => {
         <div className="filtrarProdc">
           <p className="titleProduc">
             Productos
-            <span className="filtrar">
+            <span className="filtrar" onClick={handleFiltrar}>
               Filtrar
               <span className="flecha">
                 <svg
@@ -26,114 +84,42 @@ export const Home = () => {
                 </svg>
               </span>
             </span>
+            {abrirFiltrar && (
+              <div className="cartelFiltrar">
+                <ul className="ListaFiltrado">
+                  <li className="nameFiltrar">Categoria</li>
+                  <li className="nameFiltrar">A-Z</li>
+                  <li className="nameFiltrar">Z-A</li>
+                  <li className="nameFiltrar">Descuento</li>
+                </ul>
+              </div>
+            )}
           </p>
         </div>
         <section className="contenedorProductos">
-          <section className="containerProduc">
-            <div className="containerImg">
-              <img src="./img/public4Perf.jpeg" alt="img del producto" />
-              <div className="infoProdc">
-                <p className="nameProd">Nombre del producto</p>
+          {arrayProduc.map(({ id, img, alt, nameProduc, stock, precio }) => (
+            <section className="containerProduc" key={id}>
+              <div className="containerImg">
+                <img src={img} alt={alt} />
+                <div className="infoProdc">
+                  <p className="nameProd">{nameProduc}</p>
 
-                <p className="stock">
-                  Stock disp
-                  <span className="numerStock"> 583</span>
-                </p>
+                  <p className="stock">
+                    Stock disp
+                    <span className="numerStock"> {stock}</span>
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <p className="precio">$600</p>
+              <p className="precio">${precio}</p>
 
-            <div className="contador">
-              <button className="btnContador menos">-</button>
-              <p className="numer">1</p>
-              <button className="btnContador mas">+</button>
-            </div>
-          </section>
-          <section className="containerProduc">
-            <div className="containerImg">
-              <img src="./img/i1.jpg" alt="img del producto" />
-              <div className="infoProdc">
-                <p className="nameProd">Nombre del producto</p>
-
-                <p className="stock">
-                  Stock disp
-                  <span className="numerStock"> 583</span>
-                </p>
+              <div className="contador">
+                <button className="btnContador menos">-</button>
+                <p className="numer">1</p>
+                <button className="btnContador mas">+</button>
               </div>
-            </div>
-
-            <p className="precio">$600</p>
-
-            <div className="contador">
-              <button className="btnContador menos">-</button>
-              <p className="numer">1</p>
-              <button className="btnContador mas">+</button>
-            </div>
-          </section>{" "}
-          <section className="containerProduc">
-            <div className="containerImg">
-              <img src="./img/i3.jpg" alt="img del producto" />
-              <div className="infoProdc">
-                <p className="nameProd">Nombre del producto</p>
-
-                <p className="stock">
-                  Stock disp
-                  <span className="numerStock"> 583</span>
-                </p>
-              </div>
-            </div>
-
-            <p className="precio">$600</p>
-
-            <div className="contador">
-              <button className="btnContador menos">-</button>
-              <p className="numer">1</p>
-              <button className="btnContador mas">+</button>
-            </div>
-          </section>{" "}
-          <section className="containerProduc">
-            <div className="containerImg">
-              <img src="./img/i2.jpg" alt="img del producto" />
-              <div className="infoProdc">
-                <p className="nameProd">Nombre del producto</p>
-
-                <p className="stock">
-                  Stock disp
-                  <span className="numerStock"> 583</span>
-                </p>
-              </div>
-            </div>
-
-            <p className="precio">$600</p>
-
-            <div className="contador">
-              <button className="btnContador menos">-</button>
-              <p className="numer">1</p>
-              <button className="btnContador mas">+</button>
-            </div>
-          </section>{" "}
-          <section className="containerProduc">
-            <div className="containerImg">
-              <img src="./img/public4Perf.jpeg" alt="img del producto" />
-              <div className="infoProdc">
-                <p className="nameProd">Nombre del producto</p>
-
-                <p className="stock">
-                  Stock disp
-                  <span className="numerStock"> 583</span>
-                </p>
-              </div>
-            </div>
-
-            <p className="precio">$600</p>
-
-            <div className="contador">
-              <button className="btnContador menos">-</button>
-              <p className="numer">1</p>
-              <button className="btnContador mas">+</button>
-            </div>
-          </section>
+            </section>
+          ))}
         </section>
       </section>
     </>
